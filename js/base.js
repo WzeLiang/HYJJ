c395dbafac846ccb408d452ef7b56ddd22112f5c
nomore3324 <14791286530@163.com>
2018-07-06 17:47:40 +0800

    1111

---
+++ js/base.js
@@ -0,0 +1,13 @@
+$(function () {
+    $(".content .content-tab .tab-item").eq(0).addClass("tab-active")
+    $(".content .content-box .box-list").eq(0).show().siblings().hide()
+    $(".content .content-tab .tab-item").click(function () {
+        $(this).addClass("tab-active").siblings().removeClass("tab-active")
+        var index=$(this).index()
+        $(".content .content-box .box-list").eq(index).show().siblings().hide()
+    })
+    $(".box-list .list-ul .ul-li").click(function () {
+        $(this).parents(".list-ul").css("left","-1200px")
+        $(this).parents(".box-list").find(".list-content").css("left",0)
+    })
+})
