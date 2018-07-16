0775a3a157fe33d58610806f836e98421f801a5b
nomore3324 <14791286530@163.com>
2018-07-16 18:23:47 +0800

    aaa333

--- js/base.js
+++ js/base.js
@@ -1,17 +1,13 @@
 $(function () {
-    $(".content .content-tab .tab-item").eq(0).addClass("tab-active")
-    $(".content .content-box .box-list").eq(0).show().siblings().hide()
-    $(".content .content-tab .tab-item").click(function () {
-        $(this).addClass("tab-active").siblings().removeClass("tab-active")
-        var aaa=$(this).index()
-        console.log((aaa))
-        $(".content .content-box .box-list").eq(aaa).show().siblings().hide()
-    })
-    $(".box-list .list-ul .ul-li").click(function () {
-        $(this).parents(".list-ul").css("left","-1200px")
-        $(this).parents(".box-list").find(".list-content").css("left",0)
-    })
-
+    // $(".content .content-tab .tab-item").eq(0).addClass("tab-active")
+    // $(".content .content-box .box-list").eq(0).show().siblings().hide()
+    // $(".content .content-tab .tab-item").click(function () {
+    //     $(this).addClass("tab-active").siblings().removeClass("tab-active")
+    //     var aaa=$(this).index()
+    //     console.log((aaa))
+    //     $(".content .content-box .box-list").eq(aaa).show().siblings().hide()
+    // })
+    //header tab记录
     $(".header-tablist .tab-item").removeClass("visit")
     var aa = window.sessionStorage? sessionStorage.getItem("index"): Cookie.read("index");
     $(".header-tablist .tab-item").eq(aa).addClass("visit")
@@ -20,8 +16,20 @@ $(function () {
         var bb=$(this).index();
         $(this).addClass("visit").siblings().removeClass("visit")
         sessionStorage.setItem("index", bb);
-
+    });
+    //二级菜单 tab记录
+    $(".content-tab .tab-item").removeClass("tab-active");
+    var secendindex=window.sessionStorage? sessionStorage.getItem("secendindex"): Cookie.read("secendindex");
+    if(!secendindex){
+        secendindex=0
+    }
+    $(".content-tab .tab-item").eq(secendindex).addClass("tab-active")
+    $(".content .content-box .box-list").eq(secendindex).show().siblings().hide()
+    $(".content-tab .tab-item").click(function () {
+        var ccc=$(this).index();
+        $(this).addClass("tab-active").siblings().removeClass("tab-active")
+        $(".content .content-box .box-list").eq(ccc).show().siblings().hide()
+        sessionStorage.setItem("secendindex", ccc);
 
     })
-
 })
--- js/donation.js
+++ js/donation.js
@@ -3,12 +3,14 @@ $(function () {
         $(".step-box2").css("z-index",102)
         $(".back-progress1").css("width","500px")
         $(".list-step-title .step-item .item-numb").eq(1).addClass("numb-active")
+        $(".step-box1").addClass("hidden")
 
     })
     $("#tobox3").click(function () {
         $(".step-box3").css("z-index",103)
         $(".back-progress2").css("width","500px")
         $(".list-step-title .step-item .item-numb").eq(2).addClass("numb-active")
+        $(".step-box2").addClass("hidden")
 
     })
     $(".momey-box .momey-item").eq(0).addClass("radio-active");
